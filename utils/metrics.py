1d63e19a09a05b7c81385b9e5557e6973ce5ea09
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-12-03 13:09:52 +0100

    round mean and std TRE values

--- utils/metrics.py
+++ utils/metrics.py
@@ -1,13 +1,13 @@
 import numpy as np
 
-def compute_TRE(fixed_points_file, moving_points_file, voxel_size):
+def compute_TRE(pts_inhale_file, pts_exhale_file, voxel_size):
     """
     Computes the Target Registration Error (TRE) to quantify the accuracy of the registration process. The TRE is calculated using 3D Euclidean 
     distance between the coordinates in the reference image (File 1) and the transformed coordinates in the registered image (File 2).
 
     Args:
-        fixed_points_file (str): path to the file containing the coordinates of the fixed points
-        moving_points_file (str): path to the file containing the coordinates of the moving points
+        pts_inhale_file (str): path to the file containing the coordinates of the fixed points
+        pts_exhale_file (str): path to the file containing the coordinates of the moving points
         voxel_size (float): voxel size in mm
 
     Returns:
@@ -15,14 +15,14 @@ def compute_TRE(fixed_points_file, moving_points_file, voxel_size):
         std_TRE (float): standard deviation of the TRE in mm
     """
     # load the files if paths are provided
-    fixed_points = np.loadtxt(fixed_points_file)
-    moving_points = np.loadtxt(moving_points_file)
+    pts_inhale = np.loadtxt(pts_inhale_file)
+    pts_exhale = np.loadtxt(pts_exhale_file)
 
     # Check if the number of points in both files is the same
-    if len(fixed_points) != len(moving_points):
+    if len(pts_inhale) != len(pts_exhale):
         raise ValueError("The number of points in the fixed and moving files must be the same.")
 
     # Compute the TRE
-    TRE = np.linalg.norm((fixed_points - moving_points) * voxel_size, axis=1)
+    TRE = np.linalg.norm((pts_inhale - pts_exhale) * voxel_size, axis=1)
 
-    return np.mean(TRE), np.std(TRE)
+    return np.round(np.mean(TRE),2), np.round(np.std(TRE),2)
