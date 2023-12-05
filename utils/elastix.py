20e5bc4600f2e556ab4d52689e29267a2f5d9c4e
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-12-05 23:31:51 +0100

    fix elastix utils for export paths

--- utils/elastix.py
+++ utils/elastix.py
@@ -29,6 +29,7 @@ def excute_cmd(command):
 def register_elastix(fixed_path, 
                     moving_path, 
                     reg_params, 
+                    reg_params_key,
                     create_dir_callback, 
                     excute_cmd_callback,
                     fMask = None):
@@ -39,6 +40,7 @@ def register_elastix(fixed_path,
         fixed_path ('str'): Path to the fixed image.
         moving_path ('str'): Path to the moving image.
         reg_params ('str'): Registration parameters for elastix.
+        reg_params_key ('str'): Key to identify the registration parameters output folder.
         create_dir_callback ('function'): Callback function to create directories.
         excute_cmd_callback ('function'): Callback function to execute commands.
         fMask ('str'): Optional path to a mask file.
@@ -51,7 +53,7 @@ def register_elastix(fixed_path,
     reg_moving_name = moving_path.replace("\\", "/").split("/")[-1].split(".")[0]
 
     # create output dir
-    output_dir = f'output/images/output_{reg_fixed_name}/{reg_moving_name}'
+    output_dir = f'output/{reg_params_key}/images/output_{reg_fixed_name}/{reg_moving_name}'
     create_dir_callback(output_dir)
 
     # create elastix command line
@@ -108,6 +110,7 @@ def label_propagation_transformix(
 def control_points_transformix(
     fixed_path, 
     moving_path, 
+    reg_params_key,
     input_points,
     transform_path,
     replace_text_in_file_callback,
@@ -121,10 +124,10 @@ def control_points_transformix(
     
     # Get the names of the fixed and moving images for the output directory, names without the file extensions
     reg_fixed_name  = fixed_path.replace("\\", "/").split("/")[-1].split(".")[0] 
-    reg_moving_name = os.path.join(moving_path.replace("\\", "/").split("/")[0], moving_path.replace("\\", "/").split("/")[-1].split(".")[0])
+    reg_moving_name = moving_path.replace("\\", "/").split("/")[-1].split(".")[0]
 
     # create an output directory for the labels
-    output_dir = f'output/points/output_{reg_fixed_name}/{reg_moving_name}' # rem _float64
+    output_dir = f'output/{reg_params_key}/points/output_{reg_fixed_name}/{reg_moving_name}' # rem _float64
 
     # creates the output directory
     create_dir_callback(output_dir)
