e55569258ab35b117f586d3dfac9da36276879ef
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-12-16 10:05:00 +0100

    handle single parameter file registration

--- create_script.py
+++ create_script.py
@@ -11,22 +11,34 @@ if __name__ == "__main__":
 
     parser.add_argument('--dataset_path', type=str, default='dataset/train', help='root dir for nifti training data')
     parser.add_argument('--experiment_name', type=str, default='elastix_01', help='experiment name')
-    parser.add_argument('--parameters_path', type=str, default='elastix-parameters/Par0003', help='root dir for elastix parameters.')
+    parser.add_argument('--parameters_path', type=str, default='elastix-parameters/Par0003', help='root dir for elastix parameters. The script will use all the parameters in this directory. A single .txt file can also be used.')
     parser.add_argument('--output_path', type=str, default='output', help='root dir for output scripts')
     parser.add_argument("--use_masks", action='store_true', help='if True, segmentation masks will be used during the registration.')
 
     # parse the arguments
     args = parser.parse_args()
 
-    # get the parameters from the parameters_path
-    parameters = os.listdir(args.parameters_path)
+    # check if parameters_path is .txt file
+    if os.path.isfile(args.parameters_path):
+        parameters      = args.parameters_path
 
-    if len(parameters) == 0:
-        logger.error(f"No parameters found in {args.parameters_path} directory.")
-        sys.exit(1)
+        # cteate params key folder name
+        reg_params      = '-p "{}"'.format(parameters).replace('\\', '/')
+        reg_params_key  = parameters.split('/')[-1].replace('.txt', '')
+        transform_idx   = 0
 
-    reg_params      = ' '.join(['-p "{}"'.format(os.path.join(args.parameters_path, param)) for param in parameters]).replace('\\', '/')
-    reg_params_key  = '+'.join(['{}'.format(param.replace('.txt', '')) for param in parameters])
+    elif os.path.isdir(args.parameters_path):
+        # get the parameters from the parameters_path
+        parameters = os.listdir(args.parameters_path)
+
+        if len(parameters) == 0:
+            logger.error(f"No parameters found in {args.parameters_path} directory.")
+            sys.exit(1)
+    
+        # cteate params key folder name
+        reg_params      = ' '.join(['-p "{}"'.format(os.path.join(args.parameters_path, param)) for param in parameters]).replace('\\', '/')    
+        reg_params_key  = '+'.join(['{}'.format(param.replace('.txt', '')) for param in parameters])
+        transform_idx   = len(parameters) - 1
 
     # create experiment output
     # args.experiment_name is useful to distinguish between different experiments (e.g. with or without preprocessing, etc.)
@@ -76,7 +88,7 @@ if __name__ == "__main__":
 
             # defining the control point to be transformed abd transform file paths
             input_points = os.path.join(args.dataset_path ,f'{sample_name}/{sample_name}_300_iBH_xyz_r1.txt') # i_cntl_pt
-            transform_path = f'{args.exp_output}/images/output_{i_filename_full}/{e_filename_full}/TransformParameters.{len(parameters)-1}.txt'
+            transform_path = f'{args.exp_output}/images/output_{i_filename_full}/{e_filename_full}/TransformParameters.{transform_idx}.txt'
 
             # Get the names of the fixed and moving images for the output directory, names without the file extensions
             reg_fixed_name  = fixed_path.replace("\\", "/").split("/")[-1].split(".")[0]
