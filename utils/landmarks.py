b5268510fdf806b75297b956fe797ea6a8ad3071
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-12-16 13:17:07 +0100

    experimenting landmarks visualization

--- utils/landmarks.py
+++ utils/landmarks.py
@@ -1,9 +1,7 @@
 import pandas as pd
-
-def prepare_landmarks_for_transform(landmarks_list, landmarks_file_path, output_file_path):
-    # to add the first two rows in the moving landmark file for elastix...
-
-    pass
+import numpy as np
+import matplotlib.pyplot as plt
+import nibabel as nib
 
 def write_landmarks_to_list(landmarks, file_path):
     '''
@@ -56,8 +54,41 @@ def get_landmarks_from_txt(transformed_file_path, search_key='OutputIndexFixed')
 
     return landmarks_list
 
-def visualize_landmarks():
+def visualize_landmarks(landmarks_path=None, reference_image_path=None, slice_index=70, voxel_dim=(1, 1, 1)):
     '''
-    Visualize the landmarks on the reference image.
+    Visualize the landmarks on the reference image or a mask.
     '''
-    pass
+
+    # Load the reference image
+    nii_image = nib.load(reference_image_path)
+    reference_image = nii_image.get_fdata()
+
+    # transpose the axis to rotate for visualization
+    reference_image = reference_image.transpose(2, 1, 0)
+
+    # Load 3D landmarks from the file
+    landmarks_data = np.loadtxt(landmarks_path)
+
+    # Extract x, y, z coordinates
+    x_coords = landmarks_data[:, 0].astype(int)
+    y_coords = landmarks_data[:, 1].astype(int)
+    z_coords = landmarks_data[:, 2].astype(int)
+
+    # reshsaped_landmarks = np.array([z_coords, y_coords, x_coords]).T
+    # voxel_dim = (voxel_dim[2], voxel_dim[1], voxel_dim[0])
+    # reshsaped_landmarks = reshsaped_landmarks * voxel_dim
+
+    # -1 to match the MATLAB visualizer indexing result
+    slice_index = slice_index - 1
+    
+    # Visualize a specific slice
+    plt.figure(figsize=(8, 8))
+    plt.imshow(reference_image[slice_index, :, :], cmap='gray')
+    plt.title(f"Slice {slice_index}")
+
+    # Plot landmarks on the current slice
+    plt.scatter(x_coords, y_coords, c='r', marker='+', label='Landmarks')
+    plt.title('Landmarks on Volume Slice')
+    plt.legend()
+    plt.axis('off')
+    plt.show()
