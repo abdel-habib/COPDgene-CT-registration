30c3610e4ec80d1fe6aa770426d542715bea7446
ABDELRAHMAN HABIB <abdalrhmanusama@hotmail.com>
2023-12-27 09:54:54 +0300

    improve landmarks and masks plotting - plots the mask in red, overlayed on the image, with its landmarks

--- utils/landmarks.py
+++ utils/landmarks.py
@@ -3,6 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import nibabel as nib
 import os
+from matplotlib.colors import LinearSegmentedColormap
 
 def write_landmarks_to_list(landmarks, file_path):
     '''
@@ -60,9 +61,10 @@ def visualize_landmarks(slice_index=70, subject='copd1'):
     Visualize the landmarks on the reference image or a mask.
     '''
     # Define the paths
-    landmarks_path = os.path.join(os.getcwd(),f'../dataset/train/{subject}/{subject}_300_eBH_xyz_r1.txt')
-    reference_image_path = os.path.join(os.getcwd(),f'../dataset/train/{subject}/{subject}_eBHCT_lung.nii.gz') # _lung
-    
+    landmarks_path = os.path.join(os.getcwd(),f'../dataset/train/{subject}/{subject}_300_iBH_xyz_r1.txt')
+    reference_image_path = os.path.join(os.getcwd(),f'../dataset/train/{subject}/{subject}_iBHCT.nii.gz') # _lung
+    reference_mask_path = os.path.join(os.getcwd(),f'../dataset/train/{subject}/{subject}_iBHCT_lung.nii.gz') # _lung
+
     # -1 to match the MATLAB visualizer indexing result
     slice_index = slice_index - 1
 
@@ -70,17 +72,29 @@ def visualize_landmarks(slice_index=70, subject='copd1'):
     nii_image = nib.load(reference_image_path)
     reference_image = nii_image.get_fdata()
 
+    # Load the reference mask
+    nii_mask = nib.load(reference_mask_path)
+    reference_mask = nii_mask.get_fdata()
+
     # transpose the axis to rotate for visualization
     reference_image = reference_image.transpose(2, 1, 0)
+    reference_mask = reference_mask.transpose(2, 1, 0)
     
-    # Visualize a specific slice
-    # plt.figure(figsize=(6, 6))
-    plt.imshow(reference_image[slice_index, :, :], cmap='gray')
-
     # Load 3D landmarks from the file
-    landmarks_data = np.loadtxt(landmarks_path)
+    landmarks_data = np.loadtxt(landmarks_path, skiprows=2)
     slice_landmarsk = np.array([inner_list for inner_list in landmarks_data if inner_list[2] == slice_index+1])
     
+    # Create a red-green colormap with opacity
+    cmap = LinearSegmentedColormap.from_list('red_green', ['red', 'green'], N=256)
+    
+    # Visualize a specific slice
+    plt.figure(figsize=(8, 8))
+    plt.imshow(reference_image[slice_index, :, :], cmap='gray')
+
+    # Overlay the mask with color on the landmarks
+    mask_overlay = np.ma.masked_where(reference_mask[slice_index, :, :] == 0, reference_mask[slice_index, :, :])
+    plt.imshow(mask_overlay, cmap=cmap, alpha=0.5)
+
     if len(slice_landmarsk) > 0:
             
         # Extract x, y, z coordinates
@@ -89,8 +103,10 @@ def visualize_landmarks(slice_index=70, subject='copd1'):
         z_coords = slice_landmarsk[:, 2].astype(int)
 
         # Plot landmarks on the current slice
-        plt.scatter(x_coords, y_coords, c='r', marker='+', label='Landmarks')
-        plt.legend()
-        plt.axis('off')
+        plt.scatter(x_coords, y_coords, c='b', marker='+', label=f'{landmarks_path.split("/")[-1].split(".txt")[0]} landmarks')
+
+    plt.legend()
+    plt.axis('off')
     plt.title(f"Slice {slice_index+1}")
     plt.show()
+
